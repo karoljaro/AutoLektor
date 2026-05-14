4dcc87bab02b1b26b1a156aab6793890ad48feea
karoljaro <111643413+karoljaro@users.noreply.github.com>
2026-05-14 13:36:50 +0200

    Init project

---
+++ main.py
@@ -0,0 +1,16 @@
+import asyncio
+import whisper
+
+# ==================[ Config ]==================
+VOICE: str = "pl-PL-MarekNeural"
+AUDIO_FILE = "lektor.mp3"
+# ==============================================
+
+async def create_lector() -> None:
+    pass
+
+async def main() -> None:
+    print("Hello world")
+
+if __name__ == "__main__":
+    asyncio.run(main())
